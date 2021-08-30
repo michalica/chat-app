5c717c8028c8c6778ab05a675f3a66ef3e9fae81
tomas <tomas@fitfox.de>
2021-08-30 21:16:09 +0200

    updating messages after saving, allowing different userNames via Url query  (#3)

--- src/UI/App/App.tsx
+++ src/UI/App/App.tsx
@@ -2,8 +2,11 @@ import React, {useEffect, useState} from 'react';
 import Chat from '../layouts/Chat';
 import ChatForm from '../organizms/ChatForm';
 import MessagesArea from "../organizms/MessagesArea";
-import {getAllMessages} from "../../services/Message/Message";
+import {getAllMessages, sendMessage} from "../../services/Message";
 import {MessageStructure} from "../../models/Message/Message";
+
+const DEFAULT_NAME = 'Tomas';
+
 
 function App() {
   const [messages, setMessages] = useState<MessageStructure[]>([]);
@@ -14,17 +17,43 @@ function App() {
       )
   }, [])
 
-    function sendMessage(msg: string): void
+    function getQueryByName(query: string): string | undefined
     {
+        const urlSearchParams = new URLSearchParams(window.location.search);
+        const params = Object.fromEntries(urlSearchParams.entries());
 
+        return params[query];
     }
 
+    function getAuthorName(): string
+    {
+        const name = getQueryByName('name');
+
+        if(name)
+        {
+            return name;
+        }
+        return DEFAULT_NAME;
+    }
   return (
     <Chat
-        userArea={<ChatForm />}
+        userArea={<ChatForm
+            onClick={(value: string) => {
+                sendMessage({
+                    message: value,
+                    author: getAuthorName(),
+                    timestamp: Date.now(),
+                },
+                    () => {
+                        getAllMessages(
+                            (messages: MessageStructure[]) => setMessages(messages)
+                        )
+                    }
+                );
+            }} />}
         messageArea={<div>
             <MessagesArea
-                myUserName="Tomas"
+                myUserName={getAuthorName()}
                 messages={messages}
             />
         </div>}
